cbdb3855b70f7938421334c41296a046c59c8403
fataldinazor <pushpesh.joshi141@gmail.com>
2023-08-20 16:00:21 +0530

    x5 commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const mongoose=require ("mongoose");
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views/partials'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
