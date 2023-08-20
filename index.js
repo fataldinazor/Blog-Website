09e9cda6d50668abeb2ee2c08e6f6e6c680a6026
fataldinazor <pushpesh.joshi141@gmail.com>
2023-08-20 15:54:40 +0530

    x4 commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 
 const express = require("express");
+const path=require("path")
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
@@ -8,6 +9,7 @@ const mongoose=require ("mongoose");
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
